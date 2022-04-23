d5b4b9c46c93e6844db9f5e1c35fefa73a27cf28
a.akkuzin <a.akkuzin@skbkontur.ru>
2022-04-23 16:32:36 +0400

    Add Spring framework env

---
+++ src/main/java/ru/avtomaton/wikifacts/WikiFactsApplication.java
@@ -0,0 +1,13 @@
+package ru.avtomaton.wikifacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiFactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiFactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/avtomaton/wikifacts/WikiFactsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.avtomaton.wikifacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiFactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
