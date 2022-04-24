0c444b28c1107dddcbac74370fe7541ecb3111b6
a.akkuzin <a.akkuzin@skbkontur.ru>
2022-04-24 14:33:13 +0400

    Add auth mechanism with examples (should be removed on release)

---
+++ src/main/java/ru/avtomaton/wikifacts/entity/Role.java
@@ -0,0 +1,59 @@
+package ru.avtomaton.wikifacts.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.util.Set;
+
+/**
+ * @author Anton Akkuzin
+ */
+@Entity
+@Table(name = "t_role")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role implements GrantedAuthority {
+
+    @Id
+    private Long id;
+    private String name;
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String getAuthority() {
+        return name.toString();
+    }
+
+    public enum RoleName {
+        ROLE_MEMBER,
+        ROLE_MODERATOR,
+        ROLE_ADMIN,
+    }
+}
